6f59704698473335ebb377aa008fa07a1e8f17b4
Shinho Lee <cytron1225@naver.com>
2025-10-18 18:56:34 +0900

    backend SecurityConfig Setting 01

--- backend/src/main/java/com/example/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/example/backend/config/SecurityConfig.java
@@ -1,9 +1,14 @@
 package com.example.backend.config;
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -14,6 +19,50 @@ public class SecurityConfig {
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    /**
+     * Security FilterChain
+     */
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        // CSRF 보안 필터 disable
+        http
+                .csrf(AbstractHttpConfigurer::disable);
+
+        // CORS 설정
+
+        // 기존 Form 기반 인증 필터들 disable
+        http
+                .formLogin(AbstractHttpConfigurer::disable);
+
+        // 기존 Basic 기반 인증 필터 disable
+        http
+                .httpBasic(AbstractHttpConfigurer::disable);
+
+        // 인가
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll());
+
+        // 예외 처리
+        http
+                .exceptionHandling(e -> e
+                        .authenticationEntryPoint((request, response, authException) -> {
+                            response.sendError(HttpServletResponse.SC_UNAUTHORIZED); // 401 응답
+                        })
+                        .accessDeniedHandler((request, response, authException) -> {
+                            response.sendError(HttpServletResponse.SC_FORBIDDEN); // 403 응답
+                        })
+                );
+
+        // 세션 필터 설정 (STATELESS)
+        http
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        
+        return http.build();
     }
 
 }
